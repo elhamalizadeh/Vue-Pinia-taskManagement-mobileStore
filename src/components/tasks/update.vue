47ce8406ceeb741e4b277f8a838468417fc37d14
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-01-20 00:48:01 +0330

    Update update.vue

--- src/components/tasks/update.vue
+++ src/components/tasks/update.vue
@@ -8,7 +8,7 @@
 
 <script>
 //import { useStore } from "vuex";
-import { useTaskStore } from '../../store/index';
+import { useTaskStore } from '../../store/tasks';
 import {ref} from "vue";
 
 export default {
