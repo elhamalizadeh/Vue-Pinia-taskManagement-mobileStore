43b9715c8bcc33f82e3b8d82fd36b4a9f252ff19
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-01-20 00:45:01 +0330

    Add files via upload

---
+++ src/components/cart.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+     <div class="row mt-5">
+      <div v-if="items.length == 0" class="text-center">
+      <div  style="font-size:70px">
+        <i class="bi bi-basket"></i>
+        </div>
+        <h3>Cart Is Empty</h3>
+        <router-link :to="{name:'products'}" class="btn btn-primary">Products</router-link>
+        </div>
+      <div v-else class="col-lg-12 pl-3 pt-3">
+    <table class="table table-hover border bg-white">
+        <thead>
+            <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th style="width: 10%">Quantity</th>
+            <th>SubTotal</th>
+            <th>Action></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.id">
+                <td class="align-middle">
+                    <div class="row">
+                        <div class="col-lg-2">
+                         <img
+                      :src="item.image"
+                      alt="..."
+                      class="img-fluid"
+                    />
+                        </div>
+                         <div class="col-lg-10">
+                            <h5>{{ item.id }}/{{ item.name }}</h5>
+                            <p>{{ item.description }}</p>
+                         </div>
+                    </div>
+                    </td>
+                <td class="align-middle">{{ item.price }}</td>
+                <td class="align-middle">
+                     <button @click="increment(item.id)" class="btn btn-sm btn-dark me-2">+</button>
+                   <span> {{ item.quantity }}</span>
+                    <button @click="decrement(item.id)" class="btn btn-sm btn-dark ms-2">-</button></td>
+                <td class="align-middle">{{ item.quantity * item.price }}</td>
+                 <td class="align-middle" style="width: 15%">
+                <button @click="deleteFromCart(item.id)" class="btn btn-danger btn-sm">delete</button>
+              </td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+              <td>
+                <button @click="clearAll" href="#" class="btn btn-dark">Clear Cart</button>
+              </td>
+              <td colspan="2" class="hidden-xs"></td>
+              <td class="hidden-xs text-center" style="width: 15%">
+                <strong>Total : {{ cartTotalAmount }}</strong>
+              </td>
+              <td>
+                <a href="#" class="btn btn-success btn-block">Checkout</a>
+              </td>
+            </tr>
+          </tfoot>
+    </table>
+      </div></div>
+  </div>
+</template>
+
+<script>
+import { useCartStore } from '../store/cart'
+import { computed} from "vue"
+export default {
+name: "cartIndex",
+setup(){useCartStore
+    const cartstore = useCartStore();
+    const items = computed(()=> cartstore.allItems);
+    const cartTotalAmount = computed(()=> cartstore.TotalAmount)
+
+function increment(id){
+    cartstore.plusAction(id)
+}
+function decrement(id){
+    cartstore.minusAction(id)
+}
+function deleteFromCart(id){
+  cartstore.deleteAction(id)
+}
+function clearAll(){
+  cartstore.clearAction()
+}
+
+return{ items, cartTotalAmount,increment,decrement ,deleteFromCart,clearAll}
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/products.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <h1>products</h1>
+    <div class="row align-items-center">
+    <div v-for= "product in ProductList" :key = product.id class="col-md-3 me-3 mt-3"> 
+
+      <div class="card" style="width: 18rem;">
+  <img :src="product.image" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{ product.name }}</h5>
+    <p class="card-text">{{ product.description }}</p>
+    <a href="#" class="btn btn-primary" @click="AddToCart(product)">Add To Cart</a>
+  </div>
+</div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useProductStore } from '../store/products'
+import { useCartStore } from '../store/cart'
+import { computed } from "vue"
+export default {
+name:'productsList',
+
+setup(){
+  const ProductStore = useProductStore();
+  const cartStore = useCartStore();
+
+
+  const ProductList = computed(()=>ProductStore.AllProducts)
+
+  // function getProducts(){
+  //   Products.AllProducts
+  // }
+  // getProducts();
+
+  function AddToCart(product){
+    cartStore.addToCartAction(product)
+
+  }
+
+return{ ProductList, AddToCart }
+}
+}
+</script>
+<style>
+</style>
